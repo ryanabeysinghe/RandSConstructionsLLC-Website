e1225672e3b33cf21f8e18cca9a77cf372520716
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2024-03-30 11:41:55 -0400

    home page ui finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,8 @@ module.exports = {
         matteblack: "#28282B", // Matte Black
         customBlue: "#2E95EC", // (46,149,236)
         customWhite: "#E9EBEE", // (233, 235, 238)
-        customOrange: "#d39850" // (211, 152, 80)
+        customOrange: "#d39850", // (211, 152, 80)
+        customMustardYellow: "#d39850" // (211, 152, 80)
       },
       backgroundColor: {
         'black-opacity-50': 'rgba(0, 0, 0, 0.5)',

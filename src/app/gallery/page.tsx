00d4ad5a6d807dcb45e4214f25d508905a3f06b4
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2024-05-08 19:25:12 -0400

    temp changes for gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,7 +1,18 @@
-import { Roboto } from 'next/font/google';
-import React from 'react';
+"use client"
+
+import { Monda, Roboto } from 'next/font/google';
+import React, { useState } from 'react';
+import Lightbox from "yet-another-react-lightbox";
 
 import styles from '@/styles/Gallery.module.css';
+
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/counter.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+import { Captions, Counter, Fullscreen, Slideshow, Thumbnails } from "yet-another-react-lightbox/plugins";
+import { bathroomRemodelSlides, deck_restoration_slides, basementRemodelingSlides } from '@/components/GalleryData';
 
 const robotoFont = Roboto({
   subsets: ['latin'],
@@ -9,7 +20,33 @@ const robotoFont = Roboto({
   weight: '700'
 })
 
+const mondaFont = Monda({
+  subsets: ['latin'],
+  variable: '--font-monda',
+  weight: '700'
+})
+
 const Gallery = () => {
+
+  const [open, setOpen] = useState<boolean>(false);
+
+  type LightboxKey = 'bathroomRemodeling' | 'deck_restoration' | 'basementRemodeling';
+
+  // State variable to track open/close state of each Lightbox
+  const [lightboxState, setLightboxState] = useState<{ [key in LightboxKey]: boolean }>({
+    bathroomRemodeling: false,
+    deck_restoration: false,
+    basementRemodeling: false,
+  });
+
+  // Function to toggle the state of a specific Lightbox Gallery
+  const toggleLightbox = (lightbox: LightboxKey) => {
+    setLightboxState(prevState => ({
+      ...prevState,
+      [lightbox]: !prevState[lightbox]
+    }));
+  };
+
   return (
     <main className='mt-24 overflow-x-hidden'>
 
@@ -27,10 +64,83 @@ const Gallery = () => {
       </section>
 
       {/* Gallery Picture Section */}
-      <section className='bg-matteblack py-16 xs:py-8'>
+      <section className='bg-customWhite py-16 xs:py-8'>
+        <div className="grid grid-cols-3 gap-14 px-14 xl:px-8 xl:gap-8 md:grid-cols-1 md:gap-14 sm:px-12 xs:px-10 xs:gap-8 xxs:px-8 xxxs:px-6">
 
+          {/* First Row */}
+          <div className="flex flex-col min-h-full bg-matteblack text-white p-6 w-full rounded-xl xl:h-[85%] md:col-span-1">
+            <div className='flex-grow cursor-pointer border-solid border-2 border-customBlue xl:h-[85%]'>
+              <img src={basementRemodelingSlides[0].src} alt={basementRemodelingSlides[0].title} onClick={() => toggleLightbox('basementRemodeling')} className="w-full h-full object-cover grayscale transition-all duration-500 ease-out hover:grayscale-0" />
+            </div>
 
+            <Lightbox
+              plugins={[Captions, Counter, Fullscreen, Slideshow, Thumbnails]}
+              captions={{
+                showToggle: true,
+                descriptionTextAlign: 'center'
+              }}
+              counter={{ container: { style: { top: "unset", bottom: 0 } } }}
+              open={lightboxState.basementRemodeling}
+              slides={basementRemodelingSlides}
+              close={() => toggleLightbox('basementRemodeling')}
+            />
+            
+            <div>
+              <p className={`text-3xl text-center whitespace-pre items-center pt-3 text-customMustardYellow xl:text-2xl lg:text-xl lg:truncate md:text-3xl sm:text-2xl xs:text-xl xxxs:text-lg ${mondaFont.className}`}>Basement Remodeling</p>
+            </div>
+            
+          </div>
 
+          <div className="flex flex-col min-h-full bg-matteblack text-white p-6 w-full rounded-xl xl:h-[85%] md:col-span-1">
+            <div className='flex-grow cursor-pointer border-solid border-2 border-customBlue xl:h-[85%]'>
+              <img src={deck_restoration_slides[0].src} alt={deck_restoration_slides[0].title} onClick={() => toggleLightbox('deck_restoration')} className="w-full h-full object-cover grayscale transition-all duration-500 ease-out hover:grayscale-0 overflow-hidden" />
+            </div>
+            
+
+            <Lightbox
+              plugins={[Captions, Counter, Fullscreen, Slideshow, Thumbnails]}
+              captions={{
+                showToggle: true,
+                descriptionTextAlign: 'center'
+              }}
+              counter={{ container: { style: { top: "unset", bottom: 0 } } }}
+              open={lightboxState.deck_restoration}
+              slides={deck_restoration_slides}
+              close={() => toggleLightbox('deck_restoration')}
+            />
+
+            <p className={`text-3xl text-center whitespace-pre items-center pt-3 text-customMustardYellow xl:text-2xl lg:text-xl lg:truncate md:text-3xl sm:text-2xl xs:text-xl xxxs:text-lg ${mondaFont.className}`}>Deck Restoration</p>
+          </div>
+
+          <div className="flex flex-col min-h-full bg-matteblack text-white p-6 w-full rounded-xl xl:h-[85%] md:col-span-1">
+
+          </div>
+
+          {/* Second Row */}
+          <div className="flex flex-col min-h-full bg-matteblack text-white p-6 w-full rounded-xl xl:h-[85%] md:col-span-1">
+          <div className='flex-grow cursor-pointer border-solid border-2 border-customBlue xl:h-[85%]'>
+              <img src={deck_restoration_slides[0].src} alt={deck_restoration_slides[0].title} onClick={() => toggleLightbox('deck_restoration')} className="w-full h-full object-cover grayscale transition-all duration-500 ease-out hover:grayscale-0 overflow-hidden" />
+            </div>
+            
+
+            <Lightbox
+              plugins={[Captions, Counter, Fullscreen, Slideshow, Thumbnails]}
+              captions={{
+                showToggle: true,
+                descriptionTextAlign: 'center'
+              }}
+              counter={{ container: { style: { top: "unset", bottom: 0 } } }}
+              open={lightboxState.deck_restoration}
+              slides={deck_restoration_slides}
+              close={() => toggleLightbox('deck_restoration')}
+            />
+
+            <p className={`text-3xl text-center whitespace-pre items-center pt-3 text-customMustardYellow xl:text-2xl lg:text-xl lg:truncate md:text-3xl sm:text-2xl xs:text-xl xxxs:text-lg ${mondaFont.className}`}>Deck Restoration</p>
+          </div>
+
+          
+
+        </div>
       </section>
 
     </main>
---
+++ src/components/GalleryData.tsx
@@ -0,0 +1,109 @@
+import img1 from "@/assets/gallery_hero_img.jpg";
+import test from "@/assets/test.jpg";
+
+// {
+//     src: '',
+//     title: '',
+//     description: ''
+// },
+// {
+//     src: '',
+//     title: '',
+//     description: ''
+// },
+
+
+{/* Imports for Basement Remodeling */}
+import basement_remodeling_proj_1_after_1 from "@/assets/basement_remodeling/basement_remodeling_proj_1_after_1.jpeg";
+import basement_remodeling_proj_1_before_1 from "@/assets/basement_remodeling/basement_remodeling_proj_1_before_1.jpeg";
+import basement_remodeling_proj_1_after_2 from "@/assets/basement_remodeling/basement_remodeling_proj_1_after_2.jpeg";
+import basement_remodeling_proj_1_before_2 from "@/assets/basement_remodeling/basement_remodeling_proj_1_before_2.jpeg";
+import basement_remodeling_proj_1_after_3 from "@/assets/basement_remodeling/basement_remodeling_proj_1_after_3.jpeg";
+import basement_remodeling_proj_1_before_3 from "@/assets/basement_remodeling/basement_remodeling_proj_1_before_3.jpeg";
+import basement_remodeling_proj_1_after_4 from "@/assets/basement_remodeling/basement_remodeling_proj_1_after_4.jpeg";
+import basement_remodeling_proj_1_before_4 from "@/assets/basement_remodeling/basement_remodeling_proj_1_before_4.jpeg";
+import basement_remodeling_proj_1_after_5 from "@/assets/basement_remodeling/basement_remodeling_proj_1_after_5.jpeg";
+import basement_remodeling_proj_1_before_5 from "@/assets/basement_remodeling/basement_remodeling_proj_1_before_5.jpeg";
+
+{/* Imports for Deck Installation */}
+import deck_restoration_proj_1_after_1 from "@/assets/deck_restoration/deck_restoration_proj_1_after_1.jpeg";
+import deck_restoration_proj_1_after_2 from "@/assets/deck_restoration/deck_restoration_proj_1_after_2.jpeg";
+
+export const basementRemodelingSlides = [
+    {
+        src: basement_remodeling_proj_1_after_1.src,
+        title: 'Project #1',
+        description: 'After'
+    },
+    {
+        src: basement_remodeling_proj_1_before_1.src,
+        title: 'Project #1',
+        description: 'Before'
+    },
+    {
+        src: basement_remodeling_proj_1_after_2.src,
+        title: 'Project #1',
+        description: 'After'
+    },
+    {
+        src: basement_remodeling_proj_1_before_2.src,
+        title: 'Project #1',
+        description: 'Before'
+    },
+    {
+        src: basement_remodeling_proj_1_after_3.src,
+        title: 'Project #1',
+        description: 'After'
+    },
+    {
+        src: basement_remodeling_proj_1_before_3.src,
+        title: 'Project #1',
+        description: 'Before'
+    },
+    {
+        src: basement_remodeling_proj_1_after_4.src,
+        title: 'Project #1',
+        description: 'After'
+    },
+    {
+        src: basement_remodeling_proj_1_before_4.src,
+        title: 'Project #1',
+        description: 'Before'
+    },
+    {
+        src: basement_remodeling_proj_1_after_5.src,
+        title: 'Project #1',
+        description: 'After'
+    },
+    {
+        src: basement_remodeling_proj_1_before_5.src,
+        title: 'Project #1',
+        description: 'Before'
+    },
+]
+
+export const deck_restoration_slides = [
+    {
+        src: deck_restoration_proj_1_after_1.src,
+        title: 'Project #1',
+        description: ''
+    },
+    {
+        src: deck_restoration_proj_1_after_2.src,
+        title: 'Project #1',
+        description: ''
+    },
+]
+
+export const bathroomRemodelSlides = [
+    {
+        src: test.src,
+        title: 'Project #1',
+        description: 'After'
+    },
+    {
+        src: test.src,
+        title: 'Project #1',
+        description: 'Before'
+    },
+]

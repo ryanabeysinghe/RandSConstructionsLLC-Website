b9449aaf0040f80f6947572702a833d8a1b335fa
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2024-05-15 15:01:07 -0400

    updated cover photos and responsiveness for gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -12,12 +12,20 @@ import "yet-another-react-lightbox/plugins/counter.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 import { Captions, Counter, Fullscreen, Slideshow, Thumbnails } from "yet-another-react-lightbox/plugins";
-import { 
-  deck_restoration_slides, basement_remodeling_slides, 
-  roof_replacement_slides, 
+import {
+  deck_restoration_slides, basement_remodeling_slides,
+  roof_replacement_slides,
   blinds_installation_slides,
   kitchen_remodeling_slides,
- } from '@/components/GalleryData';
+  window_installation_slides
+} from '@/components/GalleryData';
+
+import kitchen_remodeling_cover_photo from "@/assets/kitchen_remodeling/kitchen_remodeling_cover_photo.jpg";
+import blinds_installation_cover_photo from "@/assets/blinds_installation/blinds_installation_cover_photo.jpg";
+import roof_replacement_cover_photo from "@/assets/roof_replacement/roof_replacement_cover_photo.jpg";
+import deck_restoration_cover_photo from "@/assets/deck_restoration/deck_restoration_cover_photo.jpg";
+import basement_remodeling_cover_photo from "@/assets/basement_remodeling/basement_remodeling_cover_photo.jpg";
+import window_installation_cover_photo from "@/assets/window_installation/window_installation_cover_photo.jpg";
 
 const robotoFont = Roboto({
   subsets: ['latin'],
@@ -34,9 +42,10 @@ const mondaFont = Monda({
 interface GalleryComponentProps {
   imgSrc: { src: string; title: string; description: string }[];
   title: string;
+  coverPhoto: string;
 }
 
-const GalleryComponent: React.FC<GalleryComponentProps> = ({ imgSrc, title }) => {
+const GalleryComponent: React.FC<GalleryComponentProps> = ({ imgSrc, title, coverPhoto }) => {
 
   const [open, setOpen] = useState<boolean>(false);
 
@@ -45,8 +54,8 @@ const GalleryComponent: React.FC<GalleryComponentProps> = ({ imgSrc, title }) =>
 
       <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] rounded-br-3xl bg-black md:-right-3 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]' />
 
-      <img src={imgSrc[0].src} alt={imgSrc[0].title} onClick={() => setOpen(true)} className="w-full h-auto object-cover border-2 border-solid border-matteblack rounded-[0.5rem] cursor-pointer grayscale transition-all duration-500 ease-out hover:grayscale-0" />
-      
+      <img src={coverPhoto} alt={imgSrc[0].title} onClick={() => setOpen(true)} className="w-full h-auto object-cover border-2 border-solid border-matteblack rounded-[0.5rem] cursor-pointer grayscale transition-all duration-500 ease-out hover:grayscale-0" />
+
       <Lightbox
         plugins={[Captions, Counter, Fullscreen, Slideshow, Thumbnails]}
         captions={{
@@ -93,39 +102,45 @@ const Gallery = () => {
 
         <div className='w-full flex flex-col items-center justify-center'>
 
-          <div className='w-full h-full inline-block z-0 px-16 lg:px-14'>
+          <div className='w-full h-full inline-block z-0 px-16 xl:px-12 lg:px-8 sm:px-12 xs:px-10 xxs:px-8'>
 
-            <div className='grid grid-cols-12 gap-x-16 gap-y-16 lg:gap-x-14 lg:gap-y-14 md:gap-x-0 md:gap-y-20 xxs:gap-y-16'>
+            <div className='grid grid-cols-12 gap-x-16 gap-y-16 xl:gap-x-12 lg:gap-x-8 md:gap-x-0 md:gap-y-20 sm:gap-y-16'>
 
               {/* Row 1 */}
               <div className='col-span-4 md:col-span-12'>
 
-                <GalleryComponent imgSrc={blinds_installation_slides} title='Blinds Installation' />
+                <GalleryComponent imgSrc={kitchen_remodeling_slides} title='Kitchen Remodeling' coverPhoto={kitchen_remodeling_cover_photo.src} />
 
               </div>
 
               <div className='col-span-4 md:col-span-12'>
 
-                <GalleryComponent imgSrc={deck_restoration_slides} title='Deck Restoration' />
-
-              </div>
-
-              <div className='col-span-4 md:col-span-12'>
-
-                <GalleryComponent imgSrc={roof_replacement_slides} title='Roof Replacement' />
+                <GalleryComponent imgSrc={roof_replacement_slides} title='Roof Replacement' coverPhoto={roof_replacement_cover_photo.src} />
 
               </div>
 
               {/* Row 2 */}
               <div className='col-span-4 md:col-span-12'>
 
-                <GalleryComponent imgSrc={kitchen_remodeling_slides} title='Kitchen Remodeling' />
+                <GalleryComponent imgSrc={blinds_installation_slides} title='Blinds Installation' coverPhoto={blinds_installation_cover_photo.src} />
 
               </div>
-              
+
               <div className='col-span-4 md:col-span-12'>
 
-                <GalleryComponent imgSrc={basement_remodeling_slides} title='Basement Remodeling' />
+                <GalleryComponent imgSrc={window_installation_slides} title='Window Installation' coverPhoto={window_installation_cover_photo.src} />
+
+              </div>
+
+              <div className='col-span-4 md:col-span-12'>
+
+                <GalleryComponent imgSrc={deck_restoration_slides} title='Deck Restoration' coverPhoto={deck_restoration_cover_photo.src} />
+
+              </div>
+
+              <div className='col-span-4 md:col-span-12'>
+
+                <GalleryComponent imgSrc={basement_remodeling_slides} title='Basement Remodeling' coverPhoto={basement_remodeling_cover_photo.src} />
 
               </div>
 
